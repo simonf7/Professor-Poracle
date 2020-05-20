cb576baddde7b5e0dcc722edb496dff5fe289c97
Simon Foster <simonf7@hotmail.com>
2020-05-20 09:42:26 +0100

    Fix: parentID not id

--- src/events/message.js
+++ src/events/message.js
@@ -3,7 +3,7 @@ module.exports = async (client, msg) => {
   if (msg.author.bot) {
     if (
       client.config.discord.meowth.indexOf(msg.author.id) >= 0 &&
-      client.config.discord.categories.indexOf(msg.channel.id) >= 0
+      client.config.discord.categories.indexOf(msg.channel.parentID) >= 0
     ) {
       console.log('msg.id = ' + msg.id);
       msg.embeds.forEach((embed) => {
