37162f3ee79753fef15679af56fa5fe1491a7d1e
Simon Foster <simonf7@hotmail.com>
2020-05-20 12:06:51 +0100

    Feat: update status

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,7 @@ module.exports = async (client) => {
   console.log(`Commando "${client.user.tag}" awaiting for orders!`);
   client.user.setPresence({
     game: {
-      name: 'Hanging Around',
+      name: 'with Meowth',
     },
   });
   const channels = await client.channels.filter(
