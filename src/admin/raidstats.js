8472513ace3319878bc45db198225be0676f7f5b
Simon Foster <simonf7@hotmail.com>
2020-05-30 21:04:46 +0100

    Feat: default to top 10 but allow argument

--- src/admin/raidstats.js
+++ src/admin/raidstats.js
@@ -1,18 +1,28 @@
 exports.run = async (client, msg, args) => {
-  client.pool
-    .query(
-      'SELECT user_id, COUNT(*) AS total FROM dex_raidcreate GROUP BY user_id HAVING user_id IS NOT NULL ORDER BY total DESC'
-    )
-    .then((rows) => {
-      let table = [];
-      table.push(['User', 'Raids']);
-      rows.forEach(async (r) => {
-        const user =
-          client.users.get(r.user_id) || (await client.fetchUser(r.user_id));
+  let sql =
+    'SELECT user_id, COUNT(*) AS total FROM dex_raidcreate GROUP BY user_id HAVING user_id IS NOT NULL ORDER BY total DESC';
+  let limit = null;
 
-        table.push([user.username, r.total.toString()]);
-      });
+  if (args.length == 0) {
+    limit = 10;
+  }
+  if (args.length > 0 && args[0] !== 'all') {
+    limit = parseInt(args[0]);
+  }
+  if (limit) {
+    sql = sql + ' LIMIT ' + limit;
+  }
 
-      msg.reply(client.discordUtils.showTable(table, ['l', 'r']));
+  client.pool.query(sql).then((rows) => {
+    let table = [];
+    table.push(['User', 'Raids']);
+    rows.forEach(async (r) => {
+      const user =
+        client.users.get(r.user_id) || (await client.fetchUser(r.user_id));
+
+      table.push([user.username, r.total.toString()]);
     });
+
+    msg.reply(client.discordUtils.showTable(table, ['l', 'r']));
+  });
 };
