06ce6b240a05223649e5fc13e949108edd942ffb
Simon Foster <simonf7@hotmail.com>
2020-05-31 22:20:31 +0100

    Chore: remove database reference

--- src/util/migrations/2.js
+++ src/util/migrations/2.js
@@ -1,6 +1,6 @@
 exports.migrate = async (pool) => {
   await pool.query(
-    'CREATE TABLE mad.dex_raidcreate (channel_id varchar(50) NOT NULL, gym_id varchar(50) DEFAULT NULL, user_id varchar(50) DEFAULT NULL, created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CONSTRAINT dex_raidcreate_PK PRIMARY KEY (channel_id))'
+    'CREATE TABLE dex_raidcreate (channel_id varchar(50) NOT NULL, gym_id varchar(50) DEFAULT NULL, user_id varchar(50) DEFAULT NULL, created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CONSTRAINT dex_raidcreate_PK PRIMARY KEY (channel_id))'
   );
 
   return true;
