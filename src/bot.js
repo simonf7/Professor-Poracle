ae3c2b5e5f140e9081bd0dad2ff1f64d76d3c904
Simon Foster <simonf7@hotmail.com>
2020-06-28 14:04:48 +0100

    Chore: version update

--- src/bot.js
+++ src/bot.js
@@ -56,7 +56,7 @@ async function main() {
     await askToken(config);
   }
 
-  client.version = '0.9.0 (28th June 2020)';
+  client.version = '0.9.1 (28th June 2020)';
   client.config = config;
   client.dts = dts;
   client.mustache = mustache;
