563b0151fe6431d0b11d8d7ae4f2f43111219416
Simon Foster <simonf7@hotmail.com>
2020-07-02 20:17:11 +0100

    Feat: add interested stats

--- src/util/gym_utils.js
+++ src/util/gym_utils.js
@@ -1,3 +1,5 @@
+const moment = require('moment'); // require
+
 const asyncForEach = async (array, callback) => {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
@@ -111,8 +113,109 @@ const findGym = async (client, gym) => {
   return -1;
 };
 
+const getUserStats = async (client, source, limit, days) => {
+  const early = moment().format('H') < 9;
+
+  let sql = 'SELECT user_id, COUNT(*) AS total FROM ';
+  if (days <= 7) {
+    sql +=
+      '(SELECT * FROM ' +
+      source +
+      " WHERE created > '" +
+      moment()
+        .subtract(early ? days + 1 : days, 'days')
+        .format('Y-MM-DD') +
+      " 23:59:59') rc";
+  } else {
+    sql += source;
+  }
+  sql += ' GROUP BY user_id HAVING user_id IS NOT NULL ORDER BY total DESC';
+  if (limit) {
+    sql += ' LIMIT ' + limit;
+  }
+
+  return client.pool.query(sql).then(async (rows) => {
+    let userStats = null;
+
+    if (days <= 7) {
+      // get list of users
+      let users = [];
+      rows.forEach((r) => {
+        users.push("'" + r.user_id + "'");
+      });
+
+      let sql =
+        'SELECT user_id, DATE(created) AS created, COUNT(*) AS total FROM ';
+      sql +=
+        '(SELECT * FROM ' +
+        source +
+        " WHERE created > '" +
+        moment()
+          .subtract(early ? days + 1 : days, 'days')
+          .format('Y-MM-DD') +
+        " 23:59:59') rc";
+      sql +=
+        ' GROUP BY user_id, DATE(created) HAVING user_id IN (' +
+        users.join(',') +
+        ')';
+
+      userStats = await client.pool.query(sql);
+    }
+
+    let headings = ['Trainer'];
+    if (days <= 7) {
+      let now = moment();
+      if (early) {
+        now.subtract(1, 'days');
+      }
+      for (let i = 0; i < days; i++) {
+        headings.push(now.format('ddd'));
+        now.subtract(1, 'days');
+      }
+    }
+    headings.push('Total');
+    let table = [];
+    table.push(headings);
+
+    await client.asyncForEach(rows, async (r) => {
+      const user =
+        client.users.get(r.user_id) || (await client.fetchUser(r.user_id));
+
+      let data = [user.username];
+
+      if (days <= 7) {
+        let now = moment();
+        if (early) {
+          now.subtract(1, 'days');
+        }
+        for (let i = 0; i < days; i++) {
+          let found = userStats.find((v) => {
+            return (
+              r.user_id == v.user_id &&
+              now.format('D') == moment(v.created).format('D')
+            );
+          });
+          if (typeof found == 'undefined') {
+            data.push('0');
+          } else {
+            data.push(found.total.toString());
+          }
+
+          now.subtract(1, 'days');
+        }
+      }
+
+      data.push(r.total.toString());
+      table.push(data);
+    });
+
+    return table;
+  });
+};
+
 module.exports = {
   gymName,
   selectGym,
   findGym,
+  getUserStats,
 };
