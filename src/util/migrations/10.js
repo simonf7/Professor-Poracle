e97856689b43d10fcc3af31e5fd440019a4392a1
Simon Foster <simonf7@hotmail.com>
2020-07-04 21:59:11 +0100

    Chore: fixes

--- src/util/migrations/10.js
+++ src/util/migrations/10.js
@@ -2,7 +2,7 @@ const pokemonNameToNumber = require('pokemon-gif/lib/pokemon-name-to-number');
 
 exports.migrate = async (pool) => {
   await pool.query(
-    'CREATE TABLE dex_mentions (id INT auto_increment NOT NULL, channel_id varchar(50) NOT NULL, user_id varchar(50) NOT NULL, role_id varchar(50) NOT NULL, CONSTRAINT dex_mentions_PK PRIMARY KEY (id))'
+    'CREATE TABLE dex_mentions (id INT auto_increment NOT NULL, channel_id varchar(50) NOT NULL, user_id varchar(50) NOT NULL, role_id varchar(50) NOT NULL, created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CONSTRAINT dex_mentions_PK PRIMARY KEY (id))'
   );
 
   return true;
