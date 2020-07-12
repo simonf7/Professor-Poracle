5f3492b1048d779f2ee76ec5e14938bb5a8fb90c
Simon Foster <simonf7@hotmail.com>
2020-07-12 23:55:59 +0100

    Feat: respond to message edits

---
+++ src/events/messageUpdate.js
@@ -0,0 +1,12 @@
+module.exports = async (client, oldMsg, msg) => {
+  // Ignore all bots apart from Meowth
+  if (msg.author.bot) {
+    if (
+      client.config.discord.meowth.indexOf(msg.author.id) >= 0 &&
+      client.config.discord.categories.indexOf(msg.channel.parentID) >= 0
+    ) {
+      client.discordUtils.processMeowthMessage(client, msg);
+    }
+    return;
+  }
+};
