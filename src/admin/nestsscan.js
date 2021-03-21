d9c4d347f12459e09539938b961685213ad8dda3
Simon Foster <simonf7@hotmail.com>
2021-03-21 09:54:34 +0000

    Fix: update the nest scan command

--- src/admin/nestsscan.js
+++ src/admin/nestsscan.js
@@ -1,7 +1,7 @@
 exports.run = async (client, msg, args) => {
   if (args && args.length > 0) {
     if (args[0] === 'confirm') {
-      const cmd = '/home/simon/sync/update_nests.sh';
+      const cmd = 'bash /home/simon/sync/update_nests.sh';
 
       try {
         const output = execSync(cmd, { encoding: 'utf-8' }); // the default is 'buffer'
