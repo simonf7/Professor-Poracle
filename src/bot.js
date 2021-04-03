f89dd0ddcee942f3e09a6a3943ec431e7fa1b339
Simon Foster <simonf7@hotmail.com>
2021-04-03 12:36:56 +0100

    Feat: make jigglypuff shiny

--- src/bot.js
+++ src/bot.js
@@ -56,7 +56,7 @@ async function main() {
     await askToken(config);
   }
 
-  client.version = '0.21.7 (2nd April 2021)';
+  client.version = '0.21.8 (3rd April 2021)';
   client.config = config;
   client.dts = dts;
   client.mustache = mustache;
